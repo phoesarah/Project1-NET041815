1074c3852552cb7647f6d918e54452b95b23d90a
Sarah R <phoesarah@hotmail.com>
2015-04-25 08:40:19 -0500

    I don't know

---
+++ sarahrosen/Program 1/CheckPassFail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class CheckPassFail
+    {
+
+        
+        static void Main(string[] args)
+        {
+            int mark = 0;
+
+            if (mark > 50)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+
+        }
+    }
+}
