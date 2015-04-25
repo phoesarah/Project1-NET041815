90a79935cb467e4eece165a782ae351d3aed3495
Sarah R <phoesarah@hotmail.com>
2015-04-25 09:33:39 -0500

    Added third program

---
+++ sarahrosen/SumandAverage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumandAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your high number");
+            Double upperbound = Convert.ToDouble(Console.ReadLine());
+            double sum = 0;
+            double average = 0.0;
+            double count = 0;
+            for (double i = 1; i <= upperbound; i++)
+            {
+                sum = sum + i;
+                count = count + 1;
+                               
+            }
+            average = sum / count;
+            Console.WriteLine("The sum is " + Convert.ToString(sum));
+            Console.WriteLine("The Average is " + Convert.ToString(average));
+            Console.ReadLine();
+        }
+    }
+}
